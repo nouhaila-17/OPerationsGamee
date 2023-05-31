92afa9faa5db5de6b69468a76a8458f65c8fd5f2
Nouhaila <iguerdnouhaila@gmail.com>
2023-05-31 01:39:29 +0100

    working appbar

--- app/src/main/java/com/example/operationsgame/MainActivity.kt
+++ app/src/main/java/com/example/operationsgame/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 
 
